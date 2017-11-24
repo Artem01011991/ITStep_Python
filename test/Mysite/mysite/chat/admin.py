281570be4fa32142e77d71c51843dff955b3d286
Artem01011991 <31074309+Artem01011991@users.noreply.github.com>
2017-11-24 19:20:10 +0300

    reorganization

--- test/Mysite/mysite/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserAccount
-
-class UserAccountAdmin(admin.ModelAdmin):
-    list_display = ('user_photo',)
-
-
-admin.site.register(UserAccount, UserAccountAdmin)
